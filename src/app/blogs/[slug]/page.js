8f3f60e13041e8e74f734f953368409bbf60df3c
dav24github <davidthbsossa@gmail.com>
2024-09-22 22:58:23 -0400

    update

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -5,6 +5,10 @@ import Tag from "@/src/components/Elements/Tag";
 import { slug } from "github-slugger";
 import Image from "next/image";
 
+export async function generateStaticParams() {
+  return allBlogs.map((blog) => ({ slug: blog._raw.flattenedPath }));
+}
+
 export default function BlogPage({ params }) {
   const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
 
--- src/app/categories/[slug]/page.js
+++ src/app/categories/[slug]/page.js
@@ -1,6 +1,28 @@
 import { allBlogs } from "@/.contentlayer/generated";
+import BlogLayoutThree from "@/src/components/Blog/BlogLayoutThree";
 import Categories from "@/src/components/Blog/Categories";
-import { slug } from "github-slugger";
+import GithubSlugger, { slug } from "github-slugger";
+
+const slugger = new GithubSlugger();
+
+export async function generateStaticParams() {
+  const categories = [];
+  const paths = [{ slug: "all" }];
+
+  allBlogs.map((blog) => {
+    if (blog.isPublished) {
+      blog.tags.map((tag) => {
+        let slugified = slugger.slug(tag);
+        if (!categories.includes(slugified)) {
+          categories.push(slugified);
+          paths.push({ slug: slugified });
+        }
+      });
+    }
+  });
+
+  return paths;
+}
 
 const CategoryPage = ({ params }) => {
   const allCategories = ["all"];
@@ -18,12 +40,22 @@ const CategoryPage = ({ params }) => {
   });
 
   return (
-    <artice>
-      <div>
-        <h1>#{params.slug}</h1>
-        <span>Discover more categories and expand your knowledge!</span>
+    <artice className="mt-12 flex flex-col text-dark">
+      <div className="px-32 flex flex-col">
+        <h1 className="mt-6 font-semibold text-5xl">#{params.slug}</h1>
+        <span className="mt-2 inline-block">
+          Discover more categories and expand your knowledge!
+        </span>
       </div>
       <Categories categories={allCategories} active={params.slug} />
+
+      <div className="grid grid-cols-3 grid-rows-2 gap-16 mt-24 px-32">
+        {blogs.map((blog, index) => (
+          <article key={index} className="col-span-1 row-span-1 relative">
+            <BlogLayoutThree blog={blog} />
+          </article>
+        ))}
+      </div>
     </artice>
   );
 };
